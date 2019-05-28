8a6c9d20eac4af80c050f85ac67e219a8f110520
Eliud Varela <adelman.varela@gmail.com>
2019-05-27 19:25:39 -0600

    Clase de get y set de atributos, con toString e instancia de objetos

--- POO2019_P2_IntroClases/src/hn/uth/poo/p1/aplicacion/principal.java
+++ POO2019_P2_IntroClases/src/hn/uth/poo/p1/aplicacion/principal.java
@@ -5,18 +5,51 @@
  */
 package hn.uth.poo.p1.aplicacion;
 
+import hn.uth.poo.p1.objetos.Vehiculo;
+//En caso que hubiesen un monto de clases, basta con poner un asterisco donde va el nombre
+
 /**
  *
  * @author eliucinho
  */
 public class principal {
-
+    public static void saludar(String mensaje){
+        System.out.println("Hola : "+mensaje);
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        saludar("mundo");
+        
+        String placa="pdp01";
+        String color="rojo";
+        String marca="tesla";
+        String modelo="r1";
+        int anio=2018;
+        double precio=130000;
+        
+        //creando un objeto (desde ahora le van a decir instanciando un objeto)
+        Vehiculo carroNuevo=
+                new Vehiculo(placa
+                        ,color
+                        ,marca
+                        ,modelo
+                        ,anio
+                        ,precio);
+        
+        System.out.println(carroNuevo);
+        
+        Vehiculo carroConConstructorPorDefecto=
+                new Vehiculo();
+        
+        System.out.println(carroConConstructorPorDefecto);
+        
+        Vehiculo carroConPlaca=
+                new Vehiculo(placa);
+        
+        System.out.println(carroConPlaca);
     }
     
 }
--- POO2019_P2_IntroClases/src/hn/uth/poo/p1/objetos/Vehiculo.java
+++ POO2019_P2_IntroClases/src/hn/uth/poo/p1/objetos/Vehiculo.java
@@ -51,7 +51,65 @@ public class Vehiculo {
         this.precio=precio;
     }
     
+    /*********************************************
+     *  GET Y SET DE LOS ATRIBUTOS
+     *****************************************/
+    //metodo para obtener comienzan get y luego el nombre del atributo
+    public String getPlaca(){
+        return this.placa;
+    }
     
+    //los metodos de configuracion o asignacion se comienzan con el nombre set
+    public void setPlaca(String placa){
+        this.placa=placa;
+    }
     
+    public String getColor(){
+        return this.color;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
     
+    public String getMarca(){
+        return this.marca;
+    }
+    public void setMarca(String marca){
+        this.marca=marca;
+    }
+    
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String modelo){
+        this.modelo=modelo;
+    }
+    
+    public int getAnio(){
+        return this.anio;
+    }
+    public void setAnio(int anio){
+        this.anio=anio;
+    }
+    
+    public double getPrecio(){
+        return this.precio;
+    }
+    public void setPrecio(double precio){
+        this.precio=precio;
+    }
+    
+    //METODO toString: convierte el objeto a texto descriptivo
+    public String toString(){
+        String objetoConvertido=
+                "Vehiculo[%s,%s,%s,%s,%s,%s]";
+        return String.format(objetoConvertido,
+                this.placa,
+                this.color,
+                this.marca,
+                this.modelo,
+                this.anio,
+                this.precio
+                );
+    }
 }
